3ff0be1bffe2de3881b8143ec492374f032318af
Trial97 <alexandru.tripon97@gmail.com>
2023-11-06 20:32:08 +0200

    Upgraded react

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,24 +1,24 @@
 import * as React from 'react';
 type OwnProps = {
-    duration?: number;
-    auto?: boolean;
-    storageKey?: string;
-    basePath?: string;
-    filename?: string;
-    children?: any;
+  duration?: number;
+  auto?: boolean;
+  storageKey?: string;
+  basePath?: string;
+  filename?: string;
+  children?: any;
 };
 type Result = {
-    loading: boolean;
-    isLatestVersion: boolean;
-    emptyCacheStorage: (version?: string | undefined) => Promise<void>;
+  loading: boolean;
+  isLatestVersion: boolean;
+  emptyCacheStorage: (version?: string | undefined) => Promise<void>;
 };
 export declare const ClearCacheProvider: React.FC<OwnProps>;
 export declare const useClearCacheCtx: () => Result;
 export declare const useClearCache: (props?: OwnProps) => {
-    loading: boolean;
-    isLatestVersion: boolean;
-    emptyCacheStorage: (version?: string) => Promise<void>;
-    latestVersion: unknown;
+  loading: boolean;
+  isLatestVersion: boolean;
+  emptyCacheStorage: (version?: string) => Promise<void>;
+  latestVersion: any;
 };
 declare const ClearCache: React.FC<OwnProps>;
 export default ClearCache;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react';
-import createPersistedState from 'use-persisted-state';
 
 const STORAGE_KEY = 'APP_VERSION';
+
+const usePersistedState = (name: string, defaultValue: any) => {
+  const [value, setValue] = React.useState(defaultValue);
+  const nameRef = React.useRef(name);
+
+  React.useEffect(() => {
+    try {
+      const storedValue = localStorage.getItem(name);
+      if (storedValue !== null) setValue(storedValue);
+      else localStorage.setItem(name, defaultValue);
+    } catch {
+      setValue(defaultValue);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    try {
+      localStorage.setItem(nameRef.current, value);
+    } catch {}
+  }, [value]);
+
+  React.useEffect(() => {
+    const lastName = nameRef.current;
+    if (name !== lastName) {
+      try {
+        localStorage.setItem(name, value);
+        nameRef.current = name;
+        localStorage.removeItem(lastName);
+      } catch {}
+    }
+  }, [name]);
+
+  return [value, setValue];
+};
 
 const defaultProps = {
   duration: 60 * 1000,
@@ -48,8 +81,7 @@ export const useClearCache = (props?: OwnProps) => {
     ...props,
   };
   const [loading, setLoading] = React.useState(true);
-  const useAppVersionState = createPersistedState(storageKey);
-  const [appVersion, setAppVersion] = useAppVersionState('');
+  const [appVersion, setAppVersion] = usePersistedState(storageKey, '');
   const [isLatestVersion, setIsLatestVersion] = React.useState(true);
   const [latestVersion, setLatestVersion] = React.useState(appVersion);
 
@@ -64,7 +96,7 @@ export const useClearCache = (props?: OwnProps) => {
       await Promise.all(
         cacheKeys.map((key) => {
           window.caches.delete(key);
-        })
+        }),
       );
     }
 
